207d54d035e4b70cac17ca7996e214eca29bbbc6
Madoxen <pchlad5@gmail.com>
2019-03-15 21:48:57 +0100

    Revert "merge"
    This reverts commit f7e5ecd0f8ccd80e54cf0c6640e0394b460cf0e2, reversing
    changes made to 8d525f0eea4339f7fd92cab622ad3f5e2547d473.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -9,12 +8,12 @@ public class Player : MonoBehaviour
     //====== SINGLETON ======
     public static Player instance = null;
 
-    public Text TScore;
+
     long score = 0;
     public long Score
     { 
         get { return score; }
-        set { score = value; OnScoreChange(); }
+        set { score = value;  }
     }
 
 
@@ -23,18 +22,13 @@ public class Player : MonoBehaviour
     {
         if(instance == null)
         instance = this;
-
-        Score = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 
-    void OnScoreChange()
-    {
-        TScore.text = "Wynik: " + Score.ToString();
-    }
+    
 }
