7be7cacb0695cdb3995fc8fa13017b1a6a14db45
Madoxen <pchlad5@gmail.com>
2019-03-16 04:12:56 +0100

    fixed bounding boxes

---
+++ Assets/UIColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIColorChanger : MonoBehaviour
+{
+    [SerializeField]
+    Image i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Block.colorChanged += UpdateColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateColor(Color _c)
+    {
+        i.color = _c;
+    }
+}
